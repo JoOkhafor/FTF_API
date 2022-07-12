8703a5311e462ddba6529263bd031561cc11c5ce
iMac G1 <imacg1@iMac-de-Energy-20.local>
2022-07-12 10:11:21 +0000

    Update

--- routes/stats.js
+++ routes/stats.js
@@ -1,3 +1,5 @@
+// création du routeur  utile avec la Methode GET
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -26,11 +28,11 @@ router
 
 module.exports = router;
 
-// post methode
-const statsFilePath = (path.join(__dirname, './stats.json'));
+// Création de la methode POST pour créer un nouvel lecteur
+
 const createStats = async (req, res, next) => {
     try {
-      const data = fs.readFileSync(statsFilePath);
+      const data = fs.readFileSync(path.join(__dirname, './stats.json'));
       const stats = JSON.parse(data);
       const newStats = {
         id: req.body.id,
@@ -39,48 +41,69 @@ const createStats = async (req, res, next) => {
         points_scored: req.body.points_scored,
       };
       stats.push(newStats);
-      fs.writeFileSync(statsFilePath, JSON.stringify(stats));
+      fs.writeFileSync((path.join(__dirname, './stats.json')), JSON.stringify(stats));
       res.status(201).json(newStats);
     } catch (e) {
       next(e);
     }
   };
+  
+  router
+    .route('/api/v1/stats')
+    .post(createStats);
+// Créons la methode PUT 
 
-//   put methode
-const updateStats = async (req, res, next) => {
-    try {
-      const data = fs.readFileSync(statsFilePath);
-      const stats = JSON.parse(data);
-      const playerStats = stats.find(player => player.id === Number(req.params.id));
-      if (!playerStats) {
-        const err = new Error('Player stats not found');
-        err.status = 404;
-        throw err;
-      }
-      const newStatsData = {
-        id: req.body.id,
-        wins: req.body.wins,
-        losses: req.body.losses,
-        points_scored: req.body.points_scored,
-      };
-      const newStats = stats.map(player => {
-        if (player.id === Number(req.params.id)) {
-          return newStatsData;
-        } else {
-          return player;
+
+    const updateStats = async (req, res, next) => {
+        try {
+          const data = fs.readFileSync(path.join(__dirname, './stats.json'));
+          const stats = JSON.parse(data);
+          const playerStats = stats.find(player => player.id === Number(req.params.id));
+          if (!playerStats) {
+            const err = new Error('Player stats not found');
+            err.status = 404;
+            throw err;
+          }
+          const newStatsData = {
+            id: req.body.id,
+            wins: req.body.wins,
+            losses: req.body.losses,
+            points_scored: req.body.points_scored,
+          };
+          const newStats = stats.map(player => {
+            if (player.id === Number(req.params.id)) {
+              return newStatsData;
+            } else {
+              return player;
+            }
+          });
+          fs.writeFileSync((path.join(__dirname, './stats.json')), JSON.stringify(newStats));
+          res.status(200).json(newStatsData);
+        } catch (e) {
+          next(e);
         }
-      });
-      fs.writeFileSync(statsFilePath, JSON.stringify(newStats));
-      res.status(200).json(newStatsData);
-    } catch (e) {
-      next(e);
-    }
-  };
+      };
+      
+      
 
-//   Delete methode
-const deleteStats = async (req, res, next) => {
+
+
+
+
+      router
+  .route('/api/v1/stats/:id')
+  .get(getStats)
+  .put(updateStats);
+
+
+
+
+
+
+
+  const deleteStats = async (req, res, next) => {
     try {
-      const data = fs.readFileSync(statsFilePath);
+      const data = fs.readFileSync(path.join(__dirname, './stats.json'));
       const stats = JSON.parse(data);
       const playerStats = stats.find(player => player.id === Number(req.params.id));
       if (!playerStats) {
@@ -96,16 +119,22 @@ const deleteStats = async (req, res, next) => {
         }
       })
       .filter(player => player !== null);
-      fs.writeFileSync(statsFilePath, JSON.stringify(newStats));
+      fs.writeFileSync((path.join(__dirname, './stats.json')), JSON.stringify(newStats));
       res.status(200).end();
     } catch (e) {
       next(e);
     }
-  };
-  
+  }; 
+
+
+
+
+
+
+
+
   router
-    .route('/api/v1/stats')
-    .post(createStats)
-    .put(updateStats)
-    .delete(deleteStats);
-    module.exports = router;
+  .route('/api/v1/stats/:id')
+  .get(getStats)
+  .put(updateStats)
+  .delete(deleteStats);
